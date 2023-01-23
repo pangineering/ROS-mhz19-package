206e8b9bf75b2147d76661a058d8e81493ca8c04
Pangineering <pangineering@gmail.com>
2023-01-24 00:03:52 +0700

    new scripts

--- map_sensor_data/scripts/map_data.py
+++ map_sensor_data/scripts/map_data.py
@@ -1,18 +1,24 @@
-#!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
-from visualization_msgs.msg import Marker
+#! /usr/bin/env python
 
+import rospy
+from visualization_msgs.msg import Marker
+from std_msgs.msg import String
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "CO2: ", data.data)
+    print(data.data)
 
 
 def listener():
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node("rviz_marker")
+
     rospy.Subscriber("co2data", String, callback)
     rospy.spin()
+
+def concentration2color(data):
+    print(data.data)
 
 
 if __name__ == '__main__':
     listener()
+
